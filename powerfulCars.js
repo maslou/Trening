4a2ee1f7465dcf26e81f3002450eb565e3e13c57
maslou <maslin2orlin@abv.bg>
2014-09-07 16:49:21 +0300

    init rev

---
+++ powerfulCars.js
@@ -0,0 +1,8 @@
+﻿function convertKWtoHP(kW) {
+    var constHP = 1.34102209;
+    return (constHP * kW).toFixed(2);
+}
+
+console.log(convertKWtoHP(75));
+console.log(convertKWtoHP(150));
+console.log(convertKWtoHP(1000));
